99a1f7e75d09542c4455c9cdfcdfa22264517bd2
neronmoon <alistar.neron@gmail.com>
2018-06-05 14:15:31 +0200

    Init

---
+++ test/bullet_master_server/core_test.clj
@@ -0,0 +1,7 @@
+(ns bullet-master-server.core-test
+  (:require [clojure.test :refer :all]
+            [bullet-master-server.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
