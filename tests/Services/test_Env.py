377a340e8e1202f7c18b81fb0d1b30a1b8994cd5
neronmoon <alistar.neron@gmail.com>
2018-06-25 11:54:48 +0200

    Add coverage report

--- tests/Services/test_Env.py
+++ tests/Services/test_Env.py
@@ -12,7 +12,7 @@ from src.Services.Env import Env
     (" export 'a'=b", ("'a'", "b")),
     (" export 'a'=b", ("'a'", "b")),
     ("# a=b", (None, None)),
-    ("# a=b", (None, None)),
+    ("#a=b", (None, None)),
     ("a=b space ", ('a', 'b space')),
     ("a='b space '", ('a', 'b space ')),
     ('a="b space "', ('a', 'b space ')),
@@ -67,3 +67,29 @@ def test_load_dotenv_resolve():
         assert 'BAR' in os.environ
         assert 'FOO' in os.environ
         assert os.environ['FOO'] == 'BAZ'
+
+
+def test_load_dotenv_decode():
+    dotenv_path = '/tmp/.test_load_dotenv_decode'
+    with open(dotenv_path, 'w') as f:
+        f.write(u"BAR2='BAZ2'\nLAL=\"WUT\"\n\"WUT\"=LAL")
+        f.close()
+        env = Env(dotenv_path)
+        env.set_as_environment_variables()
+        assert 'BAR2' in os.environ
+        assert 'LAL' in os.environ
+        assert '"WUT"' in os.environ
+        assert os.environ['BAR2'] == "BAZ2"
+        assert os.environ['LAL'] == 'WUT'
+        assert os.environ['"WUT"'] == 'LAL'
+
+
+def test_dotenv_unicode():
+    dotenv_path = '/tmp/.test_load_dotenv_unicode'
+    with open(dotenv_path, 'w') as f:
+        f.write(u'hello="it works!😃"\nDOTENV=${hello}\n')
+        f.close()
+        env = Env(dotenv_path)
+        env.set_as_environment_variables()
+        assert 'DOTENV' in os.environ
+        assert os.environ['DOTENV'] == u'it works!😃'
