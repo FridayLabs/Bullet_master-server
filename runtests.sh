09a19a97604c48230dd38d1fc0250587685eb1a7
neronmoon <alistar.neron@gmail.com>
2018-06-29 09:35:14 +0200

    Add test report

--- runtests.sh
+++ runtests.sh
@@ -3,4 +3,9 @@
 if [ ! -e cert/cert.pem ]; then
     make certificate
 fi
-python -m pytest -v --cov-report term-missing --cov --cov-fail-under 80
+
+if [ -d "tests-reports" ]; then
+    python -m pytest -v --cov-report term-missing --cov --cov-fail-under 80 --junitxml=test-reports/junit.xml
+else
+    python -m pytest -v --cov-report term-missing --cov --cov-fail-under 80
+fi
